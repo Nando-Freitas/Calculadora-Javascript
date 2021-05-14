cd819cf8fcb183749afa68dbf41cf975e7bea7ef
Nando-Freitas <nando.freitas01@gmail.com>
2021-05-14 11:46:34 -0300

    Iniciando testes

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const {valorInicial, deletaValorInicial} = require('../js/main.js');
+var assert = require('assert');
+
+describe('Inicia Calculadora', function(){
+    it('Inicia display com 0' , function(){
+        assert.equal(valorInicial(),0);
+    });
+    it("Deleta valor inicial do display", function(){
+        assert.equal(deletaValorInicial(),"");
+    })
+});
